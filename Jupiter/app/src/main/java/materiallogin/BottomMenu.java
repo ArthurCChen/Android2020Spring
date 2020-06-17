894af71e8c6e5ea37d009a6d9138fdef5b5723d4
chuchong <33688310+chuchong@users.noreply.github.com>
2020-06-17 21:21:47 +0800

    设置好了fragment的布局

--- Jupiter/app/src/main/java/materiallogin/BottomMenu.java
+++ Jupiter/app/src/main/java/materiallogin/BottomMenu.java
@@ -1,15 +1,23 @@
 package materiallogin;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.ust.jupiter.jupiter.FragmentWanted;
 import com.ust.jupiter.jupiter.R;
-
+import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import materiallogin.ui.dashboard.DashboardFragment;
+import materiallogin.ui.home.HomeFragment;
+import materiallogin.ui.notifications.NotificationsFragment;
 
 public class BottomMenu extends AppCompatActivity {
 
@@ -21,11 +29,40 @@ public class BottomMenu extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_wanted, R.id.navigation_accepted,
+                R.id.navigation_issued, R.id.navigation_me)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.navigation_wanted:
+                        loadFragment(new HomeFragment());
+                        return true;
+                    case R.id.navigation_accepted:
+                        loadFragment(new DashboardFragment());
+                        return true;
+                    case R.id.navigation_issued:
+                        loadFragment(new NotificationsFragment());
+                        return true;
+                    case R.id.navigation_me:
+                        loadFragment(new FragmentWanted());
+                        return true;
+
+                }
+                return false;
+            }
+        });
+    }
+
+    public void loadFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.commit();
     }
 
 }
