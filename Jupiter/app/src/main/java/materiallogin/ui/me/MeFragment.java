de940311fa3977b5a149a6d6d8727212b5246988
chen-yn17 <cyn10221017@163.com>
2020-06-20 17:56:03 +0800

    修改密码、个人信息的页面设计（功能未完成，但点击按钮不会报错）

--- Jupiter/app/src/main/java/materiallogin/ui/me/MeFragment.java
+++ Jupiter/app/src/main/java/materiallogin/ui/me/MeFragment.java
@@ -1,28 +1,40 @@
 package materiallogin.ui.me;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ust.jupiter.jupiter.R;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
 
 import cn.leancloud.AVObject;
 import cn.leancloud.AVQuery;
 import cn.leancloud.AVUser;
+import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
+import materiallogin.AVODemand;
+import materiallogin.BottomMenu;
+import materiallogin.MainActivity;
+import materiallogin.SPutil;
+import android.widget.Toast;
 
 public class MeFragment extends Fragment {
 
@@ -121,5 +133,145 @@ public class MeFragment extends Fragment {
 
     private void init(Bundle savedInstanceState) {
 
+    }
+
+    public void changePassword(View view) {
+        AVUser.requestPasswordResetInBackground("tom@leancloud.rocks").blockingSubscribe();
+    }
+
+    public void logout(View view) {
+
+//        Toast.makeText(MeFragment.this, "该邮件已注册账号", Toast.LENGTH_SHORT).show();
+    }
+
+    AlertDialog alertDialog;
+    public void editMe(View view) {
+        //下方参考wxs的创建需求代码
+//        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//        final LayoutInflater inflater = getLayoutInflater();
+//        final View layout = inflater.inflate(R.layout.dialog_input_table, null);
+//        builder.setView(layout);
+//        final SPutil s = new SPutil(this);
+//        final EditText demand_title = (EditText) layout.findViewById(R.id.demand_title);
+//        final EditText demand_content = (EditText) layout.findViewById(R.id.demand_content);
+//        final EditText deadline = (EditText) layout.findViewById(R.id.deadline);
+//        final EditText wanted_number = (EditText) layout.findViewById(R.id.wanted_number);
+//        final EditText reward = (EditText) layout.findViewById(R.id.reward);
+//        final Button cancel = (Button) layout.findViewById(R.id.cancel);
+//        final Button save = (Button) layout.findViewById(R.id.save);
+//        final Spinner demand_type = (Spinner) layout.findViewById(R.id.service_type);
+//        cancel.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                if (alertDialog != null)
+//                    alertDialog.dismiss();
+//            }
+//        });
+//        save.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                boolean has_error = false;
+//                String error_content = "";
+//
+//                final String title = demand_title.getText().toString();
+//                final String content = demand_content.getText().toString();
+//                final String username = "";
+//                final String end_time = deadline.getText().toString();
+//                final String type = demand_type.getSelectedItem().toString();
+//
+//                // check title and content
+//                if (title.equals("") || content.equals("")) {
+//                    if (demand_title.getText().toString().equals("")) {
+//                        error_content = "请输入标题。";
+//                    }
+//                    else {
+//                        error_content = "请输入内容。";
+//                    }
+//                    has_error = true;
+//                }
+//                int participants_number = 0;
+//                float reward_number = 0;
+//                int days = 0;
+//                // check wanted_number
+//                try {
+//                    participants_number = Integer.parseInt(wanted_number.getText().toString());
+//                } catch (Exception e) {
+//                    error_content = "需求人数至少为一个人。";
+//                    has_error = true;
+//                }
+//                if (participants_number <= 0) {
+//                    error_content = "需求人数至少为一个人。";
+//                    has_error = true;
+//                }
+//                // check reward
+//                try {
+//                    reward_number = Float.parseFloat(reward.getText().toString());
+//                } catch (Exception e) {
+//                    error_content = "你需要承诺报酬。";
+//                    has_error = true;
+//                }
+//                // check time
+//                try {
+//                    days = Integer.parseInt(end_time);
+//                } catch (Exception e) {
+//                    error_content = "请输入一个合理的天数。";
+//                    has_error = true;
+//                }
+//                if (days <= 0) {
+//                    error_content = "任务至少持续一天。";
+//                    has_error = true;
+//                }
+//
+//                if (has_error) {
+//                    new AlertDialog.Builder(BottomMenu.this)
+//                            .setTitle("提示")
+//                            .setMessage(error_content)
+//                            .setPositiveButton("确定", null)
+//                            .show();
+//                }
+//
+//
+//                final AVODemand demand = new AVODemand();
+//                demand.setContent(content);
+//                demand.setTitle(title);
+//                demand.setUsername(username);
+//                demand.setDemand_state("inactive");
+//                demand.setWanted_number(participants_number);
+//                Calendar c = Calendar.getInstance();
+//                c.add(Calendar.DATE, days);
+//                demand.setEnd_time(c.getTime());
+//                demand.setReward(reward_number);
+//                demand.setType(type);
+//
+//                demand.saveInBackground().subscribe(new Observer<AVObject>() {
+//                    @Override
+//                    public void onSubscribe(Disposable d) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onNext(AVObject avObject) {
+//                        Toast.makeText(MeFragment.this, "发布成功", Toast.LENGTH_SHORT).show();
+//                        if (alertDialog != null)
+//                            alertDialog.dismiss();
+//                    }
+//
+//                    @Override
+//                    public void onError(Throwable e) {
+//                        System.out.println(e.getMessage());
+//                        Toast.makeText(MeFragment.this, "网络错误", Toast.LENGTH_SHORT).show();
+//                    }
+//
+//                    @Override
+//                    public void onComplete() {
+//
+//                    }
+//                });
+//            }
+//        });
+//        alertDialog = builder.create();
+//        alertDialog.show();
     }
 }
