84a0fac4090abbe232d91b32871640c33031d0d6
chen-yn17 <cyn10221017@163.com>
2020-06-21 08:44:35 +0800

    用户体系实现，预置用户初始化

--- Jupiter/app/src/main/java/materiallogin/App.java
+++ Jupiter/app/src/main/java/materiallogin/App.java
@@ -1,11 +1,15 @@
 package materiallogin;
 
 import android.app.Application;
+import android.content.Context;
 
 import cn.bmob.v3.Bmob;
 import cn.leancloud.AVLogger;
 import cn.leancloud.AVOSCloud;
+import cn.leancloud.chatkit.LCChatKit;
 import cn.leancloud.im.AVIMOptions;
+
+import materiallogin.CustomUserProvider;
 
 
 public class App extends Application {
@@ -13,6 +17,8 @@ public class App extends Application {
     @Override
     // bmob初始化
     public void onCreate() {
+        Context sContext;
+
         super.onCreate();
 //        Bmob.initialize(this,"18656acfe1e57560c20094eaaf425968");
         AVOSCloud.initialize(this,
@@ -21,5 +27,9 @@ public class App extends Application {
         AVIMOptions.getGlobalOptions().setDisableAutoLogin4Push(true);
         AVOSCloud.setLogLevel(AVLogger.Level.DEBUG);
 //        Bmob.initialize(this, "3c7d0da1ed306e604f6be4b0958f606c");
+
+        LCChatKit.getInstance().setProfileProvider(CustomUserProvider.getInstance());
+        LCChatKit.getInstance().init(getApplicationContext(), "pfwsHNbdElTmkBxq8TJ3vSnQ-MdYXbMMI", "7W0RtWNJkful7NQiBvTpB8HP", "https://dyrq8yfh.lc-cn-n1-shared.com");
+        sContext = getApplicationContext();
     }
 }
